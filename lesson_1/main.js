044161cd002a87aa75a3161c7b2a6057c7c843d0
Do Quoc An <doquocan1601@gmail.com>
2025-03-03 09:26:23 +0700

    data-type

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,12 +1,71 @@
-// Một số hàm build-in
+// Kiểu dữ liệu trong JS
 
-// 1. alert()
-// 2. console.log()
-// 3. prompt()
-// 4. confirm()
-// 5. setTimeout()
-// 6. setInterval()
+// 1. Kiểu dữ liệu nguyên thủy (Primitive data types)
+// - Number: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+// - String: 'Hello', 'Hi', 'Goodbye'
+// - Boolean: true, false
+// - Undefined: undefined
+// - Null: null
+// - Symbol
 
-var firstName = 'Do'
-var lastName = 'An'
-console.log(`${firstName} ${lastName}`)
+// 2. Kiểu dữ liệu phức tạp (Complex data types)
+// - Object: {name: 'Teo', age: 10}
+// - Function: function() {}
+
+
+//Number type
+var a = 1;
+var b = 2;
+var c = 1.5;
+
+console.log(typeof a);
+
+//String type
+var fullName = 'Quoc An \'s';
+console.log(fullName);
+console.log(typeof fullName);
+
+//Boolean type
+var isSuccess = true;
+
+//Undefined type
+var age;
+
+console.log(age);
+
+//Null type
+var isNull = null;
+console.log(isNull);
+
+//Symbol type
+var id = Symbol('id'); //unique
+var id2 = Symbol('id');
+
+console.log(typeof id);
+
+//Function type
+var myFunction = function () {
+    alert('Hi');
+}
+myFunction();
+
+//Object type
+var myObject = {
+    name: 'An',
+    age: 21,
+    address: 'Ha Noi',
+    myFunction: function () {
+        alert('Hello');
+    }
+};
+
+console.log(myObject);
+
+var myArray = [
+    'JS',
+    'PHP',
+    'Ruby'
+];
+
+console.log(myArray);
+

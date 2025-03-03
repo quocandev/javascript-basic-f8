0fdaeb962af7da2a6075b7f982e4679990e0122b
Do Quoc An <doquocan1601@gmail.com>
2025-03-03 08:47:05 +0700

    init

---
+++ lesson_1/main.js
@@ -0,0 +1,12 @@
+// Một số hàm build-in
+
+// 1. alert()
+// 2. console.log()
+// 3. prompt()
+// 4. confirm()
+// 5. setTimeout()
+// 6. setInterval()
+
+var firstName = 'Do'
+var lastName = 'An'
+console.log(`${firstName} ${lastName}`)

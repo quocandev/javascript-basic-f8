ac315431acfddbf838abb25ae8df7aa1bd2c3296
Do Quoc An <doquocan1601@gmail.com>
2025-03-08 21:46:48 +0700

    function

---
+++ lesson_1/main.js
@@ -0,0 +1,23 @@
+// Các loại function
+
+// 1. Declaration function
+// 2. Expression function
+// 3. Arrow function
+
+function showMessage() {
+    //declaration function
+}
+
+var showMessage2 = function () {
+    // expression function
+}
+
+setTimeout(function () {
+
+})
+
+var myObject = {
+    myFunction: function () {
+
+    }
+}

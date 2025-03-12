f1f2ccc6d1cde1225d05c42f40d766a405de2376
Do Quoc An <doquocan1601@gmail.com>
2025-03-12 10:34:50 +0700

    string

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,23 +1,38 @@
-// CHUỖI TRONG JAVASCRIPT
+var myString = 'Hoc js tai F8!'
 
-// 1. Tạo chuỗi
-//     - Các cách tạo chuỗi
-//     - Nên dùng cách nào? Lý do?
-//     - Kiểm tra data type
-// 2. Một số case sử dụng backslash (\)
-// 3. Xem độ dài chuỗi
-// 4. Chú ý độ dài khi viết code
-// 5. Template string ES6
+//keyword: JS string methods
 
-var fullName = 'Quoc An'
+//1. length
+console.log(myString.length);
 
-console.log(fullName.length);
+//2. find index
+console.log(myString.lastIndexOf('js'));
 
-var firstName = 'Son'
-var lastName = 'Dang'
+console.log(myString.search('js'));
 
-console.log(`Toi la: ${firstName} ${lastName}`);
+//3. cut string
+console.log(myString.slice(-3, -1));
 
+//4. replace
+console.log(myString.replace(/js/g, 'JavaScript'));
 
+//5. convert to upper case
+console.log(myString.toUpperCase());
 
+//6. convert to lower case
+console.log(myString.toLowerCase());
+
+//7. trim
+console.log(myString.trim().length);
+
+//8. split
+var languages = 'Javascript, PHP, Ruby';
+
+console.log(languages.split(', '));
+
+//9. get a character by index
+const myString2 = 'Quoc An';
+
+console.log(myString2[10]);
+console.log(myString2.charAt(0));
 

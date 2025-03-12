1a2c03b5a38c002e321d2f7346bf7f3543f906fe
Do Quoc An <doquocan1601@gmail.com>
2025-03-12 10:12:07 +0700

    master

---
+++ lesson_1/main.js
@@ -0,0 +1,23 @@
+// CHUỖI TRONG JAVASCRIPT
+
+// 1. Tạo chuỗi
+//     - Các cách tạo chuỗi
+//     - Nên dùng cách nào? Lý do?
+//     - Kiểm tra data type
+// 2. Một số case sử dụng backslash (\)
+// 3. Xem độ dài chuỗi
+// 4. Chú ý độ dài khi viết code
+// 5. Template string ES6
+
+var fullName = 'Quoc An'
+
+console.log(fullName.length);
+
+var firstName = 'Son'
+var lastName = 'Dang'
+
+console.log(`Toi la: ${firstName} ${lastName}`);
+
+
+
+

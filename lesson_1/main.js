67b81c12b52633c0248b5f1aa75e0e9375b4cf99
Do Quoc An <doquocan1601@gmail.com>
2025-03-22 21:14:48 +0700

    number

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,22 +1,24 @@
-// CHUỖI TRONG JAVASCRIPT
+// Kiểu số number trong JavaScript
 
-// 1. Tạo chuỗi
-//     - Các cách tạo chuỗi
-//     - Nên dùng cách nào? Lý do?
-//     - Kiểm tra data type
-// 2. Một số case sử dụng backslash (\)
-// 3. Xem độ dài chuỗi
-// 4. Chú ý độ dài khi viết code
-// 5. Template string ES6
+// 1. Tạo giá trị number
+// - Các cách Tạo
+// - Dùng cách nào? Tại sao?
+// - Kiểm tra data Type
 
-var fullName = 'Quoc An'
+// 2. Làm việc với number
+// - To String
+// - To Fixed
 
-console.log(fullName.length);
+//Keyword: 
 
-var firstName = 'Son'
-var lastName = 'Dang'
+var age = 18;
+var PI = 3.14;
 
-console.log(`Toi la: ${firstName} ${lastName}`);
+var otherNumber = new Number(18);
+
+var result = 20 / 1;
+
+console.log(PI.toFixed(3));
 
 
 
